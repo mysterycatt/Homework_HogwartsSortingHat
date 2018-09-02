c04d7e4b671bb6116fefeeafdeb27ba1b5c007fe
Pei Wu <peiwu@peidemacbook-air.local>
2018-09-02 10:05:52 +0800

    Initial Commit

---
+++ trueLoveHogwarts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trueLoveHogwarts
+//
+//  Created by Pei Wu on 02/09/2018.
+//  Copyright © 2018 Pei Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
